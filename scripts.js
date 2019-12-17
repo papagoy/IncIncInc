e47b042281f40ff1aeff2a4bc7d33773e83958d3
papagoy <johansonalari@gmail.com>
2019-12-17 16:26:51 +0200

    lots of additions, mobile not the best

--- scripts.js
+++ scripts.js
@@ -1,3 +1,7 @@
 document.getElementsByClassName("box zone")[0].addEventListener("click", function() {
     window.open('projects/project01.html',"_self")
 });
+
+document.getElementsByClassName("box zone")[5].addEventListener("click", function() {
+    window.open('projects/project02.html',"_self")
+});
