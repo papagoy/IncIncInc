af59b5eb995e0bfab71a76ee7f574f7769eba138
papagoy <johansonalari@gmail.com>
2019-12-18 18:30:19 +0200

    pfp

--- scripts.js
+++ scripts.js
@@ -2,6 +2,15 @@ document.getElementsByClassName("box zone")[0].addEventListener("click", functio
     window.open('projects/project01.html',"_self")
 });
 
+document.getElementsByClassName("box zone")[1].addEventListener("click", function() {
+    window.open('projects/project03.html',"_self")
+});
+
+document.getElementsByClassName("box zone")[2].addEventListener("click", function() {
+    window.open('projects/project04.html',"_self")
+});
+
+
 document.getElementsByClassName("box zone")[5].addEventListener("click", function() {
     window.open('projects/project02.html',"_self")
 });
